144436d63f3abfe316a3f0788a586e0ab4fa9b71
Bozhena <skolskabozhena@gmail.com>
2025-09-22 11:26:20 +0300

    first branch_03

--- PR2/Lab_02.cpp
+++ PR2/Lab_02.cpp
@@ -12,17 +12,17 @@ int main()
 { 
 double m; // вхідний параметр
 double n; // вхідний параметр
-//double z1; // результат обчислення 1-го виразу
+double z1; // результат обчислення 1-го виразу
 double z2; // результат обчислення 2-го виразу
 
 cout << "m = "; cin >> m;
 cout << "n = "; cin >> n;
 
-//z1 =((m - 1) * sqrt(m)-(n - 1) * sqrt(n))/(sqrt(pow(m,3)*n)+n*m+pow(m,2)-m);
-z2 = (sqrt(m) - sqrt(n)) / m;// результат z2
+z1 =((m - 1) * sqrt(m)-(n - 1) * sqrt(n))/(sqrt(pow(m,3)*n)+n*m+pow(m,2)-m);
+z2 = (sqrt(m) - sqrt(n)) / m;
 
 cout << endl;
-//cout << "z1 = " << z1 << endl;
+cout << "z1 = " << z1 << endl;
 cout << "z2 = " << z2 << endl;
 
 cin.get();
